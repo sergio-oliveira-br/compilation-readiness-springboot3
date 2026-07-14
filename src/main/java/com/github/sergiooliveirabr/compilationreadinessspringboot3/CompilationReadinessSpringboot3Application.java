5ebb7dcb444ec18da2fbe55d2a9d59b0667ea50b
Sergio <oliveira-sergio@outlook.com>
2026-07-14 16:44:06 +0100

    project created

---
+++ src/main/java/com/github/sergiooliveirabr/compilationreadinessspringboot3/CompilationReadinessSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.github.sergiooliveirabr.compilationreadinessspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompilationReadinessSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompilationReadinessSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sergiooliveirabr/compilationreadinessspringboot3/CompilationReadinessSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sergiooliveirabr.compilationreadinessspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompilationReadinessSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
